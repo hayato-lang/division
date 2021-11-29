6571132f68f017869537e2f0d5f7ea50ca2aa49a
hayato-lang <j.h.14.s.g.8@gmail.com>
2021-11-29 19:10:19 +0900

    pageの作成

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,8 @@
-
-import { BrowserRouter } from 'react-router-dom';
-import { PrimaryButton } from './components/atmos/bottun/PrimaryButton';
-import { SecondaryButton } from './components/atmos/bottun/SecondaryButton';
-import { SearchInput } from './components/molcules/SearchInput';
-import { UserCard } from './components/organisms/user/UserCard';
-import { DefaultLayout } from './components/templates/DefaultLayout';
-import { HeaderOnly } from './components/templates/HeaderOnly';
-
-const user = {
-  name: "はやと",
-  image: "https://source.unsplash.com/MOnU_o4DMQw",
-  email: "hagehage@gmail.com",
-  phone: "090-5678-1234",
-  company: {
-    name: "ハゲタコ株式会社"
-  },
-  website: "https://google.com"
-};
+import { Router } from './router/Router';
 
 function App() {
   return (
-    <BrowserRouter>
-      <DefaultLayout>
-        <PrimaryButton>ハゲタコ</PrimaryButton>
-        <SecondaryButton>検索</SecondaryButton>
-        <br />
-        <SearchInput/>
-        <UserCard user={user}/>
-      </DefaultLayout> 
-    </BrowserRouter>   
+    <Router/> 
   );
 }
 
